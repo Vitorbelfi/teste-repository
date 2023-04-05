a599ac73cea137cfecda3cf23d8d885d4572d277
Vitorbelfi <129889892+Vitorbelfi@users.noreply.github.com>
2023-04-05 13:52:31 -0300

    alteração

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ document.getElementById("botao").addEventListener("click", exibenome );
 
 document.getElementById("btn-soma").addEventListener("click", somarvalores );
 
+document.getElementById("btn-dividir").addEventListener("click", dividirvalores );
+
+document.getElementById("btn-multiplicar").addEventListener("click", multiplicarvalores );
+
+document.getElementById("btn-subtrair").addEventListener("click", subtrairvalores );
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -24,24 +30,24 @@ function somarvalores()
 }
 function dividirvalores()
 {
-    let numero1 = document.getElementById("num1").value;
-    let numero2 = document.getElementById("num2").value;
-    let numero3 = parseFloat(numero1) + parseFloat(numero2)
-    document.getElementById("resultado1").innerHTML = " = " + numero3;
+    let numero4 = document.getElementById("num4").value;
+    let numero5 = document.getElementById("num5").value;
+    let numero6 = parseFloat(numero4) / parseFloat(numero5)
+    document.getElementById("resultado2").innerHTML = " = " + numero6;
 }
 function multiplicarvalores()
 {
-    let numero1 = document.getElementById("num1").value;
-    let numero2 = document.getElementById("num2").value;
-    let numero3 = parseFloat(numero1) + parseFloat(numero2)
-    document.getElementById("resultado1").innerHTML = " = " + numero3;
+    let numero7 = document.getElementById("num7").value;
+    let numero8 = document.getElementById("num8").value;
+    let numero9 = parseFloat(numero7) * parseFloat(numero8)
+    document.getElementById("resultado3").innerHTML = " = " + numero9;
 }
 function subtrairvalores()
 {
-    let numero1 = document.getElementById("num1").value;
-    let numero2 = document.getElementById("num2").value;
-    let numero3 = parseFloat(numero1) + parseFloat(numero2)
-    document.getElementById("resultado1").innerHTML = " = " + numero3;
+    let numero10 = document.getElementById("num10").value;
+    let numero11 = document.getElementById("num11").value;
+    let numero12 = parseFloat(numero10) - parseFloat(numero11)
+    document.getElementById("resultado4").innerHTML = " = " + numero12;
 }
 
   
