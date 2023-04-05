efd34cbd3b85f8814bd714ca806e6d4c49bfd87a
Vitorbelfi <129889892+Vitorbelfi@users.noreply.github.com>
2023-04-05 11:55:19 -0300

    alteração

--- main.js
+++ main.js
@@ -13,4 +13,17 @@ function exibenome()
     document.getElementById("mensagem").innerHTML = "Meu nome é:" + usuario;
     document.getElementById("nome").value="";
 }
+document.querySelector( 'button' )
+.addEventListener( 'click', handleClick, false );
+
+function handleClick() {
+var number1 = document.querySelector( '[data-js="number1"]').value;
+var number2 = document.querySelector( '[data-js="number2"]').value;
+
+document.querySelector( '[data-js="result"]' ).innerHTML = sum( number1, number2 );
+}
+
+function sum( number1, number2 ) {
+return number1 + number2;
+}
 
