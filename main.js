91f2ead236ab744b64c567ea9e6c2f836bc915ee
Vitorbelfi <129889892+Vitorbelfi@users.noreply.github.com>
2023-04-05 12:38:42 -0300

    alteração js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function somarvalores()
     let numero1 = document.getElementById("num1").value;
     let numero2 = document.getElementById("num2").value;
     let numero3 = parseFloat(numero1) + parseFloat(numero2)
-    document.getElementById("resultado1").innerHTML = "number1:" + numero3;
+    document.getElementById("resultado1").innerHTML = "resultado1" + numero3;
 }
 function dividirvalores()
 {
